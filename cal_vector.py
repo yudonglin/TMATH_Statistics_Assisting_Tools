5ffcfa87df01ef5b357262ac7b3d1f85d4bd78c1
yudonglin <37920527+yudonglin@users.noreply.github.com>
2021-01-17 14:50:15 -0800

    bug fix

--- cal_vector.py
+++ cal_vector.py
@@ -1,40 +1,17 @@
-from vector_calculator import MathPoint, MathVector, formVectorsWithTwoPoint, workDoneBy
+from vector_calculator import *
+
+a = MathVector(-3,2,3)
+b = MathVector(3,5,-5)
+
+A = MathPoint(4,5,2)
+B = MathPoint(-6,0,2)
+
 """
-Q = MathVector(0,-1,2)
-R = MathVector(-4,-1,-3)
-P = MathVector(-1,0,-2)
-
-PQ = Q-P
-print(PQ)
-PR = R-P
-print(PR)
-crossOfPQ_PR = PQ.crossProduct(PR)
-print(crossOfPQ_PR)
-crossOfPQ_PR.magnitude()
-a = MathVector(-8,3,-2)
-b = MathVector(-1,7,3 )
-print(a.crossProduct(b))
-
-P = MathPoint(3,3,-3)
-Q = MathPoint(-1,3,-1)
-R = MathPoint(5,-3,0)
-
-
-RP = formVectorsWithTwoPoint(R,P)
-RQ = formVectorsWithTwoPoint(R,Q)
-
-v = RP.crossProduct(RQ)
-
-P = MathPoint(0,0,0)
-Q = MathPoint(-1,3,-1)
-R = MathPoint(5,-3,0)
-PQ = formVectorsWithTwoPoint(P,Q)
-PR = formVectorsWithTwoPoint(P,R)
-print(PQ)
-print(PR)
-print(PQ.crossProduct(PR))
+vector3 = formVectorsWithTwoPoint(MathPoint(4,5,0),MathPoint(-4,-2,3))#MathVector(4,-7,-4)
+print(vector3)
+vector3.print_unit_vector()
 """
-U = MathVector(1,1,0)
-P = MathVector(0,2,0)
-print(U.dot(P))
-print(U.crossProduct(P))
+
+a = MathVector(5,-4,4)
+b = MathVector(3,-2,-4)
+print(projectionOf_u_onto_v(a,b))
--- vector_calculator.py
+++ vector_calculator.py
@@ -1,37 +1,77 @@
 import math
-class MathVector:
-    def __init__(self,x,y,z=0):
-        self.x = x
-        self.y = y
-        self.z = z
-    def __str__(self):
-        return "<{0},{1},{2}>".format(self.x,self.y,self.z)
-    def __add__(self,otherVector):
-        return MathVector(self.x+otherVector.x,self.y+otherVector.y,self.z+otherVector.z)
-    def __sub__(self,otherVector):
-        return MathVector(self.x-otherVector.x,self.y-otherVector.y,self.z-otherVector.z)
-    def dot(self,otherVector):
-        return self.x*otherVector.x+self.y*otherVector.y+self.z*otherVector.z
-    def crossProduct(self,otherVector):
-        return MathVector(self.y*otherVector.z-self.z*otherVector.y,-1*(self.x*otherVector.z-self.z*otherVector.x),self.x*otherVector.y-self.y*otherVector.x)
-    def print_magnitude(self):
-        print("√"+str(self.x**2+self.y**2+self.z**2))
-    def get_magnitude(self):
-        return math.sqrt(self.x**2+self.y**2+self.z**2)
-    def copy(self):
-        return MathVector(self.x,self.y,self.z)
+import fractions
 
 class MathPoint:
     def __init__(self,x,y,z=0):
         self.x = x
         self.y = y
         self.z = z
+    def __str__(self):
+        return "<{0},{1},{2}>".format(self.x,self.y,self.z)
 
-def dotProduct(v1,v2):
+def _coordWithBracket(coord):
+    return "{"+str(coord)+"}"
+
+class MathVector(MathPoint):
+    def __init__(self,x:float,y:float,z:float=0):
+        MathPoint.__init__(self,x,y,z)
+    def __add__(self,otherVector):
+        return MathVector(
+            self.x + otherVector.x,
+            self.y + otherVector.y,
+            self.z + otherVector.z
+        )
+    def __sub__(self,otherVector):
+        return MathVector(
+            self.x - otherVector.x,
+            self.y - otherVector.y,
+            self.z - otherVector.z
+        )
+    def __mul__(self,num:int):
+        return MathVector(
+            self.x * num,
+            self.y * num,
+            self.z * num
+        )
+    def dot(self,otherVector):
+        return self.x*otherVector.x+self.y*otherVector.y+self.z*otherVector.z
+    def crossProduct(self,otherVector):
+        return MathVector(
+            self.y*otherVector.z-self.z*otherVector.y,
+            -1*(self.x*otherVector.z-self.z*otherVector.x),
+            self.x*otherVector.y-self.y*otherVector.x
+        )
+    def print_magnitude(self):
+        print("√"+str(self.x**2+self.y**2+self.z**2))
+    def print_unit_vector(self):
+        print("<\\frac{1}{0}, \\frac{2}{0}, \\frac{3}{0}>".format(self.magnitude_format
+        ,_coordWithBracket(self.x),_coordWithBracket(self.y),_coordWithBracket(self.z)))
+    @property
+    def magnitude(self):
+        return math.sqrt(self.x**2+self.y**2+self.z**2)
+    @property
+    def magnitude_sqr(self):
+        return self.x**2+self.y**2+self.z**2
+    @property
+    def magnitude_format(self):
+        return "{\sqrt{"+str(self.magnitude_sqr)+"}}"
+    def copy(self):
+        return MathVector(self.x,self.y,self.z)
+
+def dotProduct(v1,v2) -> MathVector:
     return v1.dot(v2)
 
-def formVectorsWithTwoPoint(point1,point2):
+def formVectorsWithTwoPoint(point1,point2) -> MathVector:
     return MathVector(point2.x-point1.x,point2.y-point1.y,point2.z-point1.z)
 
-def workDoneBy(force,distance):
+def workDoneBy(force,distance) -> float:
     return force.dot(distance)
+
+def angleBetween(v1,v2,inDegree=False) -> float:
+    if inDegree:
+        return math.acos(v1.dot(v2)/v1.magnitude*v2.magnitude)/math.pi*360
+    else:
+        return math.acos(v1.dot(v2)/v1.magnitude*v2.magnitude)
+
+def projectionOf_u_onto_v(v,u):
+    return v*(dotProduct(u,v)/v.magnitude_sqr)
